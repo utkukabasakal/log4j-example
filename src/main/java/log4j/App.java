ffee7c87e1e6472dbad7b18ec8b595d237e690b0
Murat Utku KABASAKALOĞLU <utkukabasakal58@gmail.com>
2019-08-15 10:35:34 +0300

    log4j example

---
+++ src/main/java/log4j/App.java
@@ -0,0 +1,15 @@
+package log4j;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class App {  static  final Logger LOGGER = Logger.getLogger(App.class);
+    public static void main( String[] args ) {
+        PropertyConfigurator.configure("C:\\Users\\Dell\\Desktop\\log4jExample\\src\\main\\resources\\log4j.properties");
+        LOGGER.debug("debug message");
+        LOGGER.info("info message");
+        LOGGER.warn("warn message");
+        LOGGER.error("error message");
+        LOGGER.fatal("fatal message");
+    }
+}
