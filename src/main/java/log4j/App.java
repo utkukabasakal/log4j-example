7ecbb297f97de33aa1e1ffd01d390000c7bd7fb3
Murat Utku KABASAKALOĞLU <utkukabasakal58@gmail.com>
2019-08-15 10:46:38 +0300

    log4j example

--- src/main/java/log4j/App.java
+++ src/main/java/log4j/App.java
@@ -3,9 +3,16 @@ package log4j;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
 public class App {  static  final Logger LOGGER = Logger.getLogger(App.class);
-    public static void main( String[] args ) {
-        PropertyConfigurator.configure("C:\\Users\\Dell\\Desktop\\log4jExample\\src\\main\\resources\\log4j.properties");
+    public static void main( String[] args ) throws IOException {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        URL url = loader.getResource("log4j.properties");
+        PropertyConfigurator.configure(url);
+
         LOGGER.debug("debug message");
         LOGGER.info("info message");
         LOGGER.warn("warn message");
